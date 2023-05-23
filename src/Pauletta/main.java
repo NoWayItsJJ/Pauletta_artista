ee85d97da37fde227378d4b4ed2a671cdcecf590
NoWayItsJJ <fabio.pauletta@gmail.com>
2023-05-23 10:48:53 +0200

    modified main

---
+++ src/Pauletta/main.java
@@ -0,0 +1,13 @@
+package Pauletta;
+
+public class main {
+	public static final int Dim_Buffer = 4;
+	
+	public static volatile int[] buffer = new int[Dim_Buffer];
+	
+
+	public static void main(String[] args) {
+		
+		}
+
+}
